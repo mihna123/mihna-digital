8d7ea9f333b2151a8ff1d3fa5941b5a4e708895f
mihna123 <mihailonvojinovic@gmail.com>
2025-05-13 22:49:33 +0200

    Add some content

---
+++ src/main.js
@@ -0,0 +1,21 @@
+import { logo } from "./logo.js";
+
+// load the logo
+
+document.querySelector("logo").innerHTML = logo;
+
+// spin the earth
+
+/**
+ * @param {String} src
+ * */
+function getNumFromSrc(src) {
+	const strNum = src.split("images/earth/")[1].split(".png")[0];
+	const num = Number.parseInt(strNum);
+	return num;
+}
+/** @type {HTMLImageElement} */
+const earthImg = document.querySelector("img.earth");
+setInterval(() => {
+	earthImg.src = `images/earth/0${(getNumFromSrc(earthImg.src) % 7) + 1}.png`;
+}, 500);
